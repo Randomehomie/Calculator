c841ce978f9b23486a07072f6d498bc75e9ad63f
Randomehomie <vignesh14m3@gmail.com>
2024-01-24 07:21:00 +0530

    .

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const display = document.getElementById('display');
 
-    // Prevent input events on the display
+    
     display.addEventListener('input', function(event) {
         event.preventDefault();
-        this.value = ''; // Clear the display
+        this.value = ''; 
     });
 });
+
+function error(){
+    try {
+        display.value = eval(display.value)
+    }
+    catch(error){
+        dsiplay.value="Error";
+    }
+
+}
